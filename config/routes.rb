5598f37798191e634cd9e7010dae2702cf0948dd
Etienne Bovet <ebovet@gmail.com>
2019-11-19 18:47:20 +0800

    Finished challenge

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  root to: 'articles#index'
+  # get 'articles/new', to: 'articles#new', as: :new
+  # get 'articles/:id', to: 'articles#show', as: :article
+  # post '/', to: 'articles#create', as: :create
+  # get 'articles/:id/edit', to: 'articles#edit', as: :edit
+  # patch '/', to: 'articles#update'
+  # delete 'articles/:id', to: 'article#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles, except: [:index]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Article.destroy_all
+10.times do
+  art = Article.new(title: Faker::Games::LeagueOfLegends.champion, content: Faker::Games::LeagueOfLegends.quote)
+  art.save
+end
